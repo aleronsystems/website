c692d0e17b7a9be23467a31599e592f419090db8
aleronsystems <bogdan@aleronsystems.com>
2026-04-24 09:43:09 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function HomePage() {
         style={{
           maxWidth: '1280px',
           margin: '0 auto',
-          marginTop: '-40px',
+          marginTop: '-60px',
           padding: '4px 28px',
           display: 'flex',
           justifyContent: 'space-between',
